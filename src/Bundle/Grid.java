fa5ae65ac366b2ff4e492ab6996cdf1561ce867d
big-pin <gracia.bruno@hotmail.com>
2014-04-04 18:40:54 -0500

    Cleaned prog

--- src/Bundle/Grid.java
+++ src/Bundle/Grid.java
@@ -15,12 +15,13 @@ public class Grid
     private final int screenY;
     private Robot r;
     private Rectangle area;
-    
+    private Tile[][] tiles;
     public Grid(Dimension resolution)
     {
         this.resolution=resolution;
         screenX=resolution.width;
         screenY=resolution.height;
+        tiles=new Tile[4][4];
         init();
     }
     
@@ -40,11 +41,11 @@ public class Grid
             yiteration:
             for(int y=300;y<320;y++)
             {
-                System.out.println(x+" "+y);
+                //System.out.println(x+" "+y);
                 if(r.getPixelColor(x, y).equals(new Color(187, 173, 160)))
                 {
-                    System.out.println("done");
-                    area=new Rectangle(x,y,500,500);
+                    log("Found game grid.");
+                    area=new Rectangle(700,310,505,500);
                     break xiteration;
                 }
             }
@@ -53,8 +54,45 @@ public class Grid
     
     public void update()
     {
-        
+        int xval=(int) (area.getX()+30);
+        int yval=(int) (area.getY()+40);
+        for(int x=0;x<4;x++)
+        {
+            for(int y=0;y<4;y++)
+            {
+                log(r.getPixelColor(xval,yval).toString());
+                tiles[x][y]=new Tile(r.getPixelColor(xval,yval),x,y);
+                log("Added tile: "+tiles[x][y].toString()+" >"+xval+","+yval);
+                yval+=120; //tile height move row
+            }
+            yval=(int) (area.getY()+30);
+            xval+=120; //move column
+        }
     }
     
+    public void showGrid()
+    {
+        update();
+        for(int y=0;y<4;y++)
+        {
+            for(int x=0;x<4;x++)
+            {
+                //System.out.println(tiles[x][y].toString());
+                
+                if(x==3)
+                {
+                    System.out.println("["+tiles[x][y].getTileValue()+"]");
+                }
+                else
+                {
+                    System.out.print("["+tiles[x][y].getTileValue()+"]");
+                }
+            }
+        }
+    }
     
+    public void log(String msg)
+    {
+        System.out.println("LOG: "+msg);
+    }
 }
--- src/Bundle/Tile.java
+++ src/Bundle/Tile.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 
 public class Tile
 {
-    private ArrayList<Color> tileColors;
+    private static ArrayList<Color> tileColors;
     Color color;
     int tileValue;
     int x;
     int y;
+    int index;
     public Tile(Color c, int x, int y)
     {
         color=c;
@@ -20,18 +21,32 @@ public class Tile
     }
     private void calcTileValue()
     {
-        int index=tileColors.indexOf(color);
+        index=tileColors.indexOf(color);
         if(index==0)
         {
             tileValue=0;
         }
         else
         {
-            tileValue=2^index;
+            tileValue=(int) Math.pow(2, index);
         }
     }
-    private void initTileColors()
-    {
+    /** 
+     * grid border=187, 173, 160
+     * blank tile= 204,192,179
+        2 tile= 238,228,218
+        4 tile= 237,224,200
+        8 tile= 242,177,121]
+        16 tile= 245,149,99
+        32 tile= 246,124,95
+        64 tile= 246,94,59
+        128 tile= 237,207,114
+        256 tile= 237,204,97
+        512 tile= 237,200,80
+    **/
+    private static void initTileColors()
+    {  
+        tileColors=new ArrayList();
         tileColors.add(new Color(204,192,179));
         tileColors.add(new Color(238,228,218));
         tileColors.add(new Color(237,224,200));
@@ -58,5 +73,18 @@ public class Tile
     public int getY()
     {
         return y;
+    }
+    public int getIndex()
+    {
+        return index;
+    }
+    @Override
+    public String toString()
+    {
+        return getTileValue()+" tile @ "+ getX()+","+getY()+" index:"+getIndex();
+    }
+    private static void log(String msg)
+    {
+        System.out.println("LOG: "+msg);
     }
 }
